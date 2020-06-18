411c97a2e4556251668582d29cec4c776951de85
Harminder virk <virk.officials@gmail.com>
2020-06-18 22:14:04 +0530

    chore: update dependencies

--- test/logger.spec.ts
+++ test/logger.spec.ts
@@ -261,7 +261,7 @@ test.group('Logger', () => {
     assert.deepEqual(logger.child({}), logger)
     assert.deepEqual(logger.bindings(), {})
     assert.isFalse(logger.isLevelEnabled('info'))
-    assert.equal(logger.pinoVersion, '6.2.1')
+    assert.equal(logger.pinoVersion, '6.3.2')
     assert.deepEqual(logger.levels, {
       labels: {
         10: 'trace',
@@ -443,7 +443,7 @@ test.group('Logger', () => {
     assert.deepEqual(logger.child({}), logger)
     assert.deepEqual(logger.bindings(), {})
     assert.isFalse(logger.isLevelEnabled('info'))
-    assert.equal(logger.pinoVersion, '6.2.1')
+    assert.equal(logger.pinoVersion, '6.3.2')
     assert.deepEqual(logger.levels, {
       labels: {
         10: 'trace',
