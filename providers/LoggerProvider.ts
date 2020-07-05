3319c0931a13a1675ad16f174d24286ca1e17855
Harminder virk <virk.officials@gmail.com>
2020-07-05 17:34:46 +0530

    chore: add prettier

--- providers/LoggerProvider.ts
+++ providers/LoggerProvider.ts
@@ -1,21 +1,21 @@
 /*
-* @adonisjs/logger
-*
-* (c) Harminder Virk <virk@adonisjs.com>
-*
-* For the full copyright and license information, please view the LICENSE
-* file that was distributed with this source code.
-*/
+ * @adonisjs/logger
+ *
+ * (c) Harminder Virk <virk@adonisjs.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 import { IocContract } from '@adonisjs/fold'
 import { Logger } from '../src/Logger'
 
 export default class LoggerProvider {
-  constructor (protected container: IocContract) {}
+	constructor(protected container: IocContract) {}
 
-  public register () {
-    this.container.singleton('Adonis/Core/Logger', () => {
-      return new Logger(this.container.use('Adonis/Core/Config').get('app.logger', {}))
-    })
-  }
+	public register() {
+		this.container.singleton('Adonis/Core/Logger', () => {
+			return new Logger(this.container.use('Adonis/Core/Config').get('app.logger', {}))
+		})
+	}
 }
--- src/getPino.ts
+++ src/getPino.ts
@@ -1,11 +1,11 @@
 /*
-* @adonisjs/logger
-*
-* (c) Harminder Virk <virk@adonisjs.com>
-*
-* For the full copyright and license information, please view the LICENSE
-* file that was distributed with this source code.
-*/
+ * @adonisjs/logger
+ *
+ * (c) Harminder Virk <virk@adonisjs.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 /// <reference path="../adonis-typings/logger.ts" />
 
@@ -15,7 +15,7 @@ import { LoggerConfig } from '@ioc:Adonis/Core/Logger'
 /**
  * Returns an instance of pino logger by adjusting the config options
  */
-export function getPino (options: LoggerConfig): Pino.Logger {
-  const pinoOptions = Object.assign({}, options)
-  return options.stream ? Pino(pinoOptions as any, options.stream) : Pino(pinoOptions as any)
+export function getPino(options: LoggerConfig): Pino.Logger {
+	const pinoOptions = Object.assign({}, options)
+	return options.stream ? Pino(pinoOptions as any, options.stream) : Pino(pinoOptions as any)
 }
