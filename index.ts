d57e7335066bd6fbf1c640229f548055403835bc
Harminder Virk <virk.officials@gmail.com>
2022-11-11 15:36:21 +0530

    refactor: export LoggerManager

--- index.ts
+++ index.ts
@@ -10,6 +10,7 @@
 export { Logger } from './src/logger.js'
 export { targets } from './src/targets/main.js'
 export { defineConfig } from './src/define_config.js'
+export { LoggerManager } from './src/logger_manager.js'
 export {
   transport,
   destination,
